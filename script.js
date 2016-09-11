72485f1aa8fe2621b649f2261aa36ce93f28cc68
yseredich <yseredich@gmail.com>
2016-09-11 23:32:50 +0300

    add json load

--- script.js
+++ script.js
@@ -6,33 +6,64 @@ var container = document.querySelector('#ansContainer');
 var questContainer = container.querySelector('#quest');
 var answer = container.querySelectorAll('input[name=answer]');
 var answerLabel = container.querySelectorAll('.answer-lbl');
+var counterContainer = container.querySelector('#counter');
 var btn = container.querySelector('.btn');
-var item = {
-    quest: 'Что я делаю по выходным',
-    ans1: 'хожу в парк',
-    ans2: 'хожу в бар',
-    ans3: 'страдаю',
-    ans4: 'сплю весь день',
-    right: 'ans3'
-};
-function setQuiz(data) {
-    questContainer.innerHTML = data.quest;
-    for (var i=1; i <= answer.length; i++){
-        answerLabel[i-1].innerHTML = data['ans' + i];
-        if (answer[i-1].id == item.right) {
-            answer[i-1].setAttribute('data-right', 'true');
-        }
-    }
-}
-setQuiz(item);
+var questions = [];
+var rightScore = 0;
+answer[0].checked = true;
 
+getQuestions();
+
+var j = 0;
+setQuiz(questions[j], 1);
 btn.onclick = function(e) {
     e.preventDefault();
     var right = container.querySelector('input[data-right=true]');
     if (right.checked) {
-        alert('yes');
+        rightScore += 1;
+    }
+    j++;
+    if (j < questions.length) {
+        setQuiz(questions[j], j+1);
     } else {
-        alert('donkey');
+        alert('Твой счет ' + rightScore + ' из ' + questions.length + ', нубасина!');
     }
 };
+
+
+
+function setQuiz(data, counter) {
+    questContainer.innerHTML = data.question;
+    var answerText = data.answers;
+    for (var i = 0; i < answer.length; i++){
+        answerLabel[i].innerHTML = answerText[i];
+        answer[i].setAttribute('data-right', 'false');
+        if (answer[i].id == 'ans' + data.right) {
+            answer[i].setAttribute('data-right', 'true');
+        }
+    }
+    answer[0].checked = true;
+    counterContainer.innerHTML = counter;
+}
+
+function getQuestions() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'questions.json', false);
+    //xhr.timeout = 10000;
+    container.classList.add('questions-loading');
+    xhr.onload = function() {
+        var response = xhr.responseText;
+        questions = JSON.parse(response);
+        container.classList.remove('questions-loading');
+    };
+    xhr.ontimeout = function() {
+        container.classList.add('questions-loading-failure');
+    };
+    xhr.onerror = function() {
+        container.classList.add('questions-loading-failure');
+    };
+    xhr.send();
+}
+
+
 
